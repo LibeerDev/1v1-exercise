8654d47e96ae603f47f8b9c696589190738b7976
LibeerDev <libeergithub@gmail.com>
2023-09-22 22:49:38 +0200

    added autoplay

---
+++ script.js
@@ -0,0 +1,11 @@
+const dnb1 = document.getElementById('dnb1');
+
+dnb1.volume = 0.2;
+
+document.addEventListener("mousemove", () => {
+    dnb1.play();
+})
+
+document.addEventListener("click", () => {
+    dnb1.play();
+})
