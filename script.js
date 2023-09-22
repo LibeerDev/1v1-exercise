17c064fae92c7f7c9fec96ec69596eb2d3045b4d
LibeerDev <libeergithub@gmail.com>
2023-09-22 23:31:12 +0200

    playaudio on mobile

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ document.addEventListener("mousemove", () => {
 document.addEventListener("click", () => {
     dnb1.play();
 })
+
+// Check if the device is a mobile phone
+function isMobile() {
+    return /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+  
+  // Function to handle touch event
+  function handleTouchStart(event) {
+    // Your code to handle the touch event goes here
+    dnb1.play();
+  }
+  
+  // Add the event listener if the device is a mobile phone
+  if (isMobile()) {
+    document.addEventListener('touchstart', handleTouchStart);
+  }
